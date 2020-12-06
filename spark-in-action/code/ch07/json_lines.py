b3e23d30c4da020a663c2c0d1b7b07b15bb4e94e
Carl Hinderer <carl.hinderer4@gmail.com>
2020-12-05 21:37:53 -0700

    Added example of extracting fields directly from a JSON Lines document.

--- spark-in-action/code/ch07/json_lines.py
+++ spark-in-action/code/ch07/json_lines.py
@@ -1,20 +1,35 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
 import os
 
 PARENT_DIR = '/home/carl/Code/Hadoop/spark/spark-in-action/code'
 FORCLOSURE_PATH = "ch07/data/durham-nc-foreclosure-2006-2016.json"
 JSON_FILE_PATH = os.path.join(PARENT_DIR, FORCLOSURE_PATH)
 
+
 # Creates a session on a local master
 spark = SparkSession.builder.appName("JSON Lines to Dataframe").getOrCreate()
 
 
+# Load the JSON Lines file
 df = spark.read.format("json") \
         .load(JSON_FILE_PATH)
 
+
+# Access the JSON fields directly
+new_df = df.withColumn("year", col("fields.year")) \
+           .withColumn("coordinates", col("geometry.coordinates"))
+
+
 # Shows at most 5 rows from the dataframe
 df.show(5)
-
 df.printSchema()
 
+
+# Display the extracted JSON fields
+new_df.show(5)
+new_df.printSchema()
+
+
+# Stop the session
 spark.stop()
